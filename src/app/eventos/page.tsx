cf0183d8a689fbd79df78ec96bfa13ca0c085084
predicador2023 <josebedoya2013@gmail.com>
2026-02-26 09:15:27 -0300

    deploy4

--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation"; // ✅ para leer query params
